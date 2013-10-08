eee0af8d4556677e45aded3c87bbf5e0efded5a8
Nestor Alvarez <alu0100597943@ull.edu.es>
2013-10-08 14:42:59 +0100

    Definicion del trabajo

---
+++ suma_mult.rb
@@ -0,0 +1,25 @@
+=begin
+Practica 04 de LPP
+Autores:
+	Francisco J. Alderete González
+	Néstor Álvarez Díaz
+
+
+Funciones a definir:
+	rand_matrix(d): crea un array de dxd
+			y lo rellena con Rand
+
+	show_matrix(m): muestra la matriz que se
+			le pasa por parametros
+
+	op_suma(a,b):	realiza la suma de las
+			matrices a y b
+
+	op_multi(a,b):	realiza la multiplicacion
+			de las matrices a y b
+
+	menu:		un menu para seleccionar
+			si queremos sumar o multiplicar
+
+=end
+
